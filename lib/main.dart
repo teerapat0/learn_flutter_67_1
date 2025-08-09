39694bedfe770b001abd63e4c60600cbf5cd3751
unknown <6749010011@phuketvc.ac.th>
2025-08-09 15:14:15 +0700

    step3

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,21 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text("Hello Flutter! Step2");
+    //return Text("Hello Flutter! Step2");
+    return Center(
+      child: Container(
+        color: Colors.blueAccent,
+        margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        padding: EdgeInsets.all(40),
+        child: Text(
+          "Hello Flutter! Step3",
+          style: TextStyle(
+            fontSize: 30,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
   }
 }
